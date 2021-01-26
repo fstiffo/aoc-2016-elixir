285c72ca6fc6d502645ca3c55e7f496a30c31463
fstiffo <fstiffo@sgajo.com>
2021-01-26 11:01:30 +0100

    Second hlaf solution day 9

--- lib/aoc/day09.ex
+++ lib/aoc/day09.ex
@@ -27,5 +27,24 @@ defmodule Aoc.Day09 do
   end
 
   def solve2(input) do
+    decompressed_len(input)
+  end
+
+  def decompressed_len(t), do: decompressed_len(t, 0)
+  def decompressed_len("", len), do: len
+
+  def decompressed_len(t, len) do
+    capture = Regex.run(~r/^([^(]*)\(([^)x]+)x([^)]+)\)(.*)$/, t)
+
+    if is_nil(capture) do
+      decompressed_len("", len + String.length(t))
+    else
+      [_, no_mrks, mrk_chars, mrk_times, rest] = capture
+      {:ok, subseqs_re} = Regex.compile("^(.{" <> mrk_chars <> "})(.*)$")
+      [_, subseqs, rest] = Regex.run(subseqs_re, rest)
+      times = String.to_integer(mrk_times)
+      no_mrks_len = String.length(no_mrks)
+      decompressed_len(rest, len + no_mrks_len + times * decompressed_len(subseqs))
+    end
   end
 end
