eb394e2d1f92fef45ba4156e2745df8f1b992fe2
fstiffo <fstiffo@sgajo.com>
2021-01-27 10:23:34 +0100

    Complete solution day 10

--- lib/aoc/day10.ex
+++ lib/aoc/day10.ex
@@ -7,16 +7,11 @@ defmodule Aoc.Day10 do
   def run(2), do: solve2(get_input(10))
 
   def solve1(input) do
-    read_instructions(input)
-    # :ets.delete(:instructions)
-  end
+    input
+    |> read_instructions
+    |> zooming_around()
 
-  defmodule Bot do
-    defstruct [:low, :high]
-  end
-
-  defmodule InOut do
-    defstruct [:mc]
+    :ets.delete(:instructions)
   end
 
   def read_instructions(t) do
@@ -31,23 +26,29 @@ defmodule Aoc.Day10 do
             devs,
             "bot#{bot}",
             case Map.get(devs, "bot#{bot}") do
-              nil -> struct(Bot, %{low: mc})
-              b -> struct(b, %{high: mc})
+              nil -> [String.to_integer(mc)]
+              b -> [String.to_integer(mc) | b]
             end
           )
 
         ["bot", bot, "gives", "low", "to", l_dest, l_n, "and", "high", "to", h_dest, h_n] ->
-          :ets.insert(:instructions, {bot, [to_low: l_dest <> l_n, to_high: h_dest <> h_n]})
+          :ets.insert(
+            :instructions,
+            {"bot" <> bot, [low_to: l_dest <> l_n, high_to: h_dest <> h_n]}
+          )
+
           devs
       end
     end)
   end
 
-  def run(devs) do
+  def zooming_around(devs) do
+    # IO.inspect(devs)
+
     two_mcs =
       Enum.filter(devs, fn
-        {_, %{high: h, low: l}} -> not is_nil(l) and not is_nil(h)
-        {_, %{mc: _}} -> false
+        {_, [_, _]} -> true
+        {_, _} -> false
       end)
 
     if two_mcs == [] do
@@ -56,16 +57,41 @@ defmodule Aoc.Day10 do
       new_devs = devs
 
       two_mcs
-      |> Enum.reduce(new_devs, fn {bot, _}, ds ->
-        {_, [to_low: to_low, to_high: to_high]} = :ets.lookup(:instructions, bot)
-        to_low_d = Map.get(devs, to_low)
-        to_high_d = Map.get(devs, to_low)
+      |> Enum.reduce(new_devs, fn {bot, [mc1, mc2]}, ds ->
+        if [17, 61] == [mc1, mc2] or [61, 17] == [mc1, mc2], do: IO.inspect(bot)
+        low = min(mc1, mc2)
+        high = max(mc1, mc2)
+        [{_, [low_to: low_to, high_to: high_to]}] = :ets.lookup(:instructions, bot)
 
-        Map.put(new_devs, )
+        ds =
+          case Map.get(ds, low_to) do
+            nil -> Map.put(ds, low_to, [low])
+            b -> Map.put(ds, low_to, [low | b])
+          end
+
+        ds =
+          case Map.get(ds, high_to) do
+            nil -> Map.put(ds, high_to, [high])
+            b -> Map.put(ds, high_to, [high | b])
+          end
+
+        Map.put(ds, bot, [])
       end)
+      |> zooming_around
     end
   end
 
   def solve2(input) do
+    devs =
+      input
+      |> read_instructions
+      |> zooming_around()
+
+    [mc0] = Map.get(devs, "output0")
+    [mc1] = Map.get(devs, "output1")
+    [mc2] = Map.get(devs, "output2")
+    IO.inspect(mc0 * mc1 * mc2)
+
+    :ets.delete(:instructions)
   end
 end
